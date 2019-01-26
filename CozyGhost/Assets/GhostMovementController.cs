2efafcb68a58e3a908b4516ce156f2b1dc52deb3
Andrew Decker <andrewdecker1@live.com>
2019-01-25 23:29:55 -0500

    Initial Commit of Project

---
+++ CozyGhost/Assets/GhostMovementController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GhostMovementController : MonoBehaviour
+{
+    public static GhostMovementController Instance = null;
+
+    public float Speed = 1f;
+    
+    public Rigidbody2D RB { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        RB.MovePosition(RB.position + movement * Speed * Time.fixedDeltaTime);
+    }
+}
---
+++ CozyGhost/Assets/PlayerCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Vector2 playerBounds;
+    public Vector2 focuspoint;
+
+    private Rigidbody2D RB;
+
+    public void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        focuspoint = Vector2.zero;
+    }
+
+    public void FixedUpdate()
+    {
+
+        Vector2 diff = GhostMovementController.Instance.RB.position - RB.position;
+        if(Mathf.Abs(diff.x) > playerBounds.x || Mathf.Abs(diff.y) > playerBounds.y)
+            RB.MovePosition(RB.position + diff * GhostMovementController.Instance.Speed * Time.fixedDeltaTime);
+    }
+}
